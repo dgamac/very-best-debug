1b82a3c3d3d32181ed885c0a41337b46a7ff2a07
dgamac <165616196+dgamac@users.noreply.github.com>
2024-05-09 16:37:21 +0000

    controllers

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,49 +1,57 @@
 class VenuesController < ApplicationController
 
   def index
-    matching_venues = venue.all
-    venues = matching_venues.order(:created_at)
+    matching_venues = Venue.all
+    @venues = matching_venues.order(:created_at)
 
     render({ :template => "venue_templates/venue_list" })
   end
 
   def show
-    venue_id = params.fetch("venue_id")
-    matching_venues = Venue.where({ :id => venue_id })
-    the_venue = matching_venues
-
+    id = params.fetch("venue_id")
+    @the_venue = Venue.where({ :id => id })[0]
     render({ :template => "venue_templates/details" })
   end
 
   def create
     @venue = Venue.new
-    venue.address = params.fetch("query_address")
-    venue.name = params.fetch("name")
-    venue.neighborhood = params.fetch("neighborhood")
-    venue.save
+    @venue.address = params.fetch("query_address")
+    @venue.name = params.fetch("query_name")
+    @venue.neighborhood = params.fetch("query_neighborhood")
 
-    redirect_to("/venues/#{venue.name}")
+    if  @venue.valid?
+      @venue.save
+      redirect_to("/venues/#{@venue.id}", { :notice => "Venue posted successfully." })
+    else
+      redirect_to("/venues", { :notice => "Venue failed to post successfully." })
+    end
+
   end
   
   def update
     the_id = params.fetch("venue_id")
 
-    @venue = Venue.where({ :id => the_id })
-    venue.address = params.fetch("query_address")
-    venue.name = params.fetch("Query_name")
-    venue.neighborhood = params.fetch("query_neighborhood")
-    venue.save
+    @venue = Venue.where({ :id => the_id })[0]
+    @venue.address = params.fetch("query_address")
+    @venue.name = params.fetch("query_name")
+    @venue.neighborhood = params.fetch("query_neighborhood")
+
+      if  @venue.valid?
+      @venue.save
+      redirect_to("/venues/#{@venue.id}", { :notice => "Venue updated successfully." })
+    else
+      redirect_to("/venues", { :notice => "Venue failed to update successfully." })
+    end
     
-    redirect_to("/venues/#{venue.id}")
   end
 
   def destroy
     the_id = params.fetch("venue_id")
-    matching_venues = Venue.where({ :id => the_id })
-    venue = matching_venues
-    venue.destroy
+    @venue = Venue.where({ :id => the_id })[0]
+    
+    @venue.destroy
 
-    redirect_to("/venues")
+    redirect_to("/venues", { :notice => "Venue deleted successfully."} )
   end
 
 end
